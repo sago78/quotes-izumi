55ac0171e8f788aec217add6992440c08e61e18f
Julio Sanchez <julio-antonio.sanchez@outlook.com>
2020-12-26 12:30:41 -0600

    Initial commit

---
+++ src/main/java/com/sagonet/quotesizumi/QuotesIzumiApplication.java
@@ -0,0 +1,13 @@
+package com.sagonet.quotesizumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesIzumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesIzumiApplication.class, args);
+	}
+
+}
